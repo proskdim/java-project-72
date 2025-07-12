a789eca556e88726e9d3b88dd2b292053df72d14
proskurekov <proskurekovdim@gmail.com>
2025-07-12 15:39:46 +0700

    refactoring

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
--- app/src/main/java/hexlet/code/repository/UrlRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlRepository.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
