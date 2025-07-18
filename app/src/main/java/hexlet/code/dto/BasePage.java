847e4756c9743f10b4aa46d4e7e8afc5a4b77190
proskurekov <proskurekovdim@gmail.com>
2025-07-18 20:39:10 +0700

    lint

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -3,7 +3,6 @@ package hexlet.code.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
